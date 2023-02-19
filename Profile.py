e5603f013f8a957b5c06959ec2e3369cbb996dea
Alec's Laptop <alecjsommerhauser@gmail.com>
2023-02-18 21:35:12 -0800

    messanger commit

--- Profile.py
+++ Profile.py
@@ -1,23 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image
 import base64
-import random
-from io import BytesIO
 import socket
-
-
-def get_image():
-    root = tk.Tk()
-    root.withdraw()
-    file_path = filedialog.askopenfilename()
-
-    with open(file_path, 'rb') as image_file:
-        image_content = image_file.read()
-        encoded_image = base64.b64encode(image_content)
-        image_str = encoded_image.decode('utf-8')
-
-    return image_str
 
 class Account:
     email = ''
@@ -30,15 +14,11 @@ class Profile(Account):
     spokenLanguages = []
     learningLanguage = ''
     country = ''
-    image = get_image()
     age = 0
     phoneNumber = 0
-    netCode = 0
     matches = []
     likes = []
     dislikes = []
-
-
 
     def __init__(self, name, spokenLanguages, learningLanguage, country, email, password, age, phoneNumber, netCode):
         self.name = name
@@ -51,3 +31,15 @@ class Profile(Account):
         self.phoneNumber = phoneNumber
         self.netCode = netCode
         self.ipaddress = socket.gethostbyname(socket.gethostname())
+
+    def get_image(self):
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename()
+
+        with open(file_path, 'rb') as image_file:
+            image_content = image_file.read()
+            encoded_image = base64.b64encode(image_content)
+            image_str = encoded_image.decode('utf-8')
+
+        self.image = image_str
